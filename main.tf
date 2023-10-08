45e7bfe3c5a5df5a13625c33d1b662cb12507026
Nitish Kumar <nitishkumar@outlook.com>
2023-10-08 11:31:04 +0530

    updated with user data

--- main.tf
+++ main.tf
@@ -1,30 +1,41 @@
+# Create SSH key by using below command
+# ssh-keygen -t rsa
+# Enter file in which to save the key (/Users/nitishkumar/.ssh/id_rsa): ./id_rsa
+
+resource "aws_key_pair" "key-tf" {
+  key_name   = "key-tf"
+  public_key = file("${path.module}/id_rsa.pub")
+}
+
+output "key_name" {
+  value = aws_key_pair.key-tf.key_name
+}
+
 # EC2 Instance to host Jenkins
 resource "aws_instance" "JenkinsEc2" {
   ami           = var.ami
   instance_type = var.instancetype
-  key_name      = var.instance_keypair
-  # Commented below as we are using null resorce to install Jenkins.sh file
-  # user_data     = file("${path.module}/jenkins.sh")
+  key_name      = aws_key_pair.key-tf.key_name
   vpc_security_group_ids = [aws_security_group.vpc_web.id]
   tags = {
     "Name" = "JenkinsEC2"
   }
 
   # User Data
-  user_data = file("${path.module}/jenkins_tfcloud.sh")
+  user_data = file("${path.module}/jenkins.sh")
 }
 
 # EIP
-resource "aws_eip" "myeip" {
-  # instance = aws_instance.JenkinsEc2.id
-  vpc      = true
-}
+# resource "aws_eip" "myeip" {
+#   instance = aws_instance.JenkinsEc2.id
+#   vpc      = true
+# }
 
 # EIP Association
-resource "aws_eip_association" "eip_asso" {
-  instance_id = aws_instance.JenkinsEc2.id
-  allocation_id = aws_eip.myeip.id
-}
+# resource "aws_eip_association" "eip_asso" {
+#   instance_id = aws_instance.JenkinsEc2.id
+#   allocation_id = aws_eip.myeip.id
+# }
 
 # Create Security Group - Web Traffic
 resource "aws_security_group" "vpc_web" {
@@ -58,34 +69,34 @@ resource "aws_security_group" "vpc_web" {
 
 # Create null resorce to insatll the Jenkins shell script through provisioner
 
-resource "null_resource" "myprovisioner" {
+# resource "null_resource" "myprovisioner" {
 
-  # ssh into the EC2 instance
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    host        = aws_instance.JenkinsEc2.public_ip
-    private_key = file("${path.module}/userdata/terraform-key.pem")
-    # private_key = "/terraform-key.pem"
-  }
+#   # ssh into the EC2 instance
+#   connection {
+#     type        = "ssh"
+#     user        = "ec2-user"
+#     host        = aws_instance.JenkinsEc2.public_ip
+#     private_key = file("${path.module}/userdata/terraform-key.pem")
+#     # private_key = "/terraform-key.pem"
+#   }
 
 
-  # copy the jenkins.sh file to EC2 instance using file provisioner
-  provisioner "file" {
-    source      = "jenkins_tfcloud.sh"
-    destination = "/tmp/jenkins_tfcloud.sh"
-  }
+#   # copy the jenkins.sh file to EC2 instance using file provisioner
+#   provisioner "file" {
+#     source      = "jenkins_tfcloud.sh"
+#     destination = "/tmp/jenkins_tfcloud.sh"
+#   }
 
-  # Set permission and run the jenkins.sh file using remote provisioner
-  provisioner "remote-exec" {
-    inline = [
-      "sudo chmod +x /tmp/jenkins_tfcloud.sh",
-      "sh /tmp/jenkins_tfcloud.sh"
-    ]
-  }
+#   # Set permission and run the jenkins.sh file using remote provisioner
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo chmod +x /tmp/jenkins_tfcloud.sh",
+#       "sh /tmp/jenkins_tfcloud.sh"
+#     ]
+#   }
 
-  # Lifecycle dependency
-  depends_on = [
-    aws_instance.JenkinsEc2
-  ]
-}
+#   # Lifecycle dependency
+#   depends_on = [
+#     aws_instance.JenkinsEc2
+#   ]
+# }
